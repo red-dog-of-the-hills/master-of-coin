c64c072f7dcc4d888ff1d7f98e093725d81ac13c
Red Dog Of The Hills <230697980+red-dog-of-the-hills@users.noreply.github.com>
2026-03-07 12:22:29 +0530

    Removed unused argument

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,8 @@
 import os
 
-import pytest
-
 from api.config import Environment, get_config
 
 
-def pytest_sessionstart(session: pytest.Session) -> None:
+def pytest_sessionstart() -> None:
     get_config.cache_clear()
     os.environ["APP_ENV"] = Environment.TEST.value
